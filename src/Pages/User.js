b246206d6438205e80b9d0251ee07ae1a8cd60d9
tejasraibagi26 <tejasraibagi@gmail.com>
2021-08-12 11:16:04 +0530

    Added option to add new expenses

--- src/Pages/User.js
+++ src/Pages/User.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../Components/Navbar";
 import "../CSS/user.css";
 import { FaArrowRight, FaPlus } from "react-icons/fa";
+import { HiPencilAlt, HiTag } from "react-icons/hi";
 
 var transactions = require("../Data/transaction.json");
 var transaction = transactions["transactions"];
-transaction.map((t) => console.log(t));
+
+var types = require("../Data/types.json");
+console.log(types);
 
 export default function User() {
   const [navOpen, setNavOpen] = useState(false);
   const [greeting, setGreeting] = useState("");
+  const [addExpense, setAddExpense] = useState(false);
   var user = localStorage.getItem("user");
 
   useEffect(() => {
@@ -22,6 +26,11 @@ export default function User() {
     else if (currentHr < 18) setGreeting("Good Afternoon");
     else setGreeting("Good Evening");
   }, []);
+
+  var onAddExpense = () => {
+    setAddExpense(!addExpense);
+  };
+
   return (
     <>
       <Navbar
@@ -35,6 +44,79 @@ export default function User() {
         <h1 className="greeting">
           {greeting}, {user}.
         </h1>
+        {addExpense ? (
+          <div className="bgFil">
+            <div className="add-expense blur">
+              <div className="modal-container">
+                <div className="title">Add Expense</div>
+                <div className="form" style={{ marginTop: "20px" }}>
+                  <div className="inp">
+                    <label className="lable">
+                      <HiPencilAlt /> Expense
+                      <input
+                        placeholder="Enter Title of Expense"
+                        // onChange={onValueChanged}
+                        name="expense"
+                        className="textfield"
+                        type="text"
+                        required
+                        style={{
+                          width: "100%",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          display: "block",
+                        }}
+                      />
+                    </label>
+                  </div>
+                  <div className="inp">
+                    <label
+                      className="lable"
+                      style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                        display: "block",
+                      }}
+                    >
+                      <HiTag /> Type
+                      <select name="type" id="type" className="eType">
+                        {types.map((type) => (
+                          <option value={type["value"]} id={type["id"]}>
+                            {type["value"]}
+                          </option>
+                        ))}
+                      </select>
+                    </label>
+                  </div>
+                  <div className="inp">
+                    <label className="lable">
+                      <HiPencilAlt /> Amount
+                      <input
+                        placeholder="Amount of Expense"
+                        // onChange={onValueChanged}
+                        name="expense"
+                        className="textfield"
+                        type="number"
+                        required
+                        style={{
+                          width: "100%",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          display: "block",
+                        }}
+                      />
+                    </label>
+                  </div>
+                  <div className="btn" onClick={onAddExpense}>
+                    <div className="btn-title">Add Expense</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <> </>
+        )}
         <div className="container">
           <div className="left-container">
             <div className="row">
@@ -71,7 +153,7 @@ export default function User() {
             </div>
           </div>
         </div>
-        <div className="add">
+        <div className="add" onClick={onAddExpense}>
           <div className="cirBtn">
             <FaPlus className="add-icon " />
           </div>
